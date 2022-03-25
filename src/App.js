09c4e70f55fad65a4be1c898c90396f8b4efafc0
Thomas White <thomas.white.ucf@gmail.com>
2022-03-25 11:42:20 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// , { useState, useEffect }
+import "./App.css";
+
+
+const App = () => {
+
+  return (
+    <div className="app">
+      <h1>Thomas Resume</h1>
+    </div>
+  );
+};
+
+export default App;
